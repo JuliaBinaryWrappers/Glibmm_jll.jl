5a4cfd3ca82364193a4c55aac08ff210f13e97ca
jlbuild <juliabuildbot@gmail.com>
2023-05-03 14:49:47 +0000

    Glibmm_jll build 2.66.6+0

--- src/wrappers/x86_64-apple-darwin-cxx11.jl
+++ src/wrappers/x86_64-apple-darwin-cxx11.jl
@@ -4,26 +4,26 @@ export libgiomm, libglibmm, libglibmm_generate_extra_defs
 using Glib_jll
 using libsigcpp_jll
 JLLWrappers.@generate_wrapper_header("Glibmm")
-JLLWrappers.@declare_library_product(libgiomm, "@rpath/libgiomm-2.68.1.dylib")
-JLLWrappers.@declare_library_product(libglibmm, "@rpath/libglibmm-2.68.1.dylib")
-JLLWrappers.@declare_library_product(libglibmm_generate_extra_defs, "@rpath/libglibmm_generate_extra_defs-2.68.1.dylib")
+JLLWrappers.@declare_library_product(libgiomm, "@rpath/libgiomm-2.4.1.dylib")
+JLLWrappers.@declare_library_product(libglibmm, "@rpath/libglibmm-2.4.1.dylib")
+JLLWrappers.@declare_library_product(libglibmm_generate_extra_defs, "@rpath/libglibmm_generate_extra_defs-2.4.1.dylib")
 function __init__()
     JLLWrappers.@generate_init_header(Glib_jll, libsigcpp_jll)
     JLLWrappers.@init_library_product(
         libgiomm,
-        "lib/libgiomm-2.68.1.dylib",
+        "lib/libgiomm-2.4.1.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libglibmm,
-        "lib/libglibmm-2.68.1.dylib",
+        "lib/libglibmm-2.4.1.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libglibmm_generate_extra_defs,
-        "lib/libglibmm_generate_extra_defs-2.68.1.dylib",
+        "lib/libglibmm_generate_extra_defs-2.4.1.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
--- src/wrappers/x86_64-linux-musl-cxx03.jl
+++ src/wrappers/x86_64-linux-musl-cxx03.jl
@@ -4,26 +4,26 @@ export libgiomm, libglibmm, libglibmm_generate_extra_defs
 using Glib_jll
 using libsigcpp_jll
 JLLWrappers.@generate_wrapper_header("Glibmm")
-JLLWrappers.@declare_library_product(libgiomm, "libgiomm-2.68.so.1")
-JLLWrappers.@declare_library_product(libglibmm, "libglibmm-2.68.so.1")
-JLLWrappers.@declare_library_product(libglibmm_generate_extra_defs, "libglibmm_generate_extra_defs-2.68.so.1")
+JLLWrappers.@declare_library_product(libgiomm, "libgiomm-2.4.so.1")
+JLLWrappers.@declare_library_product(libglibmm, "libglibmm-2.4.so.1")
+JLLWrappers.@declare_library_product(libglibmm_generate_extra_defs, "libglibmm_generate_extra_defs-2.4.so.1")
 function __init__()
     JLLWrappers.@generate_init_header(Glib_jll, libsigcpp_jll)
     JLLWrappers.@init_library_product(
         libgiomm,
-        "lib/libgiomm-2.68.so",
+        "lib/libgiomm-2.4.so",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libglibmm,
-        "lib/libglibmm-2.68.so",
+        "lib/libglibmm-2.4.so",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libglibmm_generate_extra_defs,
-        "lib/libglibmm_generate_extra_defs-2.68.so",
+        "lib/libglibmm_generate_extra_defs-2.4.so",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
